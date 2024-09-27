277e8ae56cddcdb2b3a1fb037905854cf2c07efe
Jey <codaaiteam@gmail.com>
2024-09-26 22:45:11 -0400

    paprer mine game update csp

--- next.config.js
+++ next.config.js
@@ -10,7 +10,12 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;"
+            value: "default-src 'self'; " +
+                   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://scratch.mit.edu; " +
+                   "frame-src https://scratch.mit.edu; " +
+                   "style-src 'self' 'unsafe-inline' https://scratch.mit.edu; " +
+                   "img-src 'self' data: https://scratch.mit.edu; " +
+                   "connect-src 'self' https://scratch.mit.edu;"
           },
         ],
       },
