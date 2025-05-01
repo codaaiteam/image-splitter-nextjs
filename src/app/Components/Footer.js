3e711f793466c9e69fe71ee07937b59d2c0377e7
codaaiteam <codaaiteam@gmail.com>
2025-04-30 23:00:43 -0400

    更新文件

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -86,6 +86,11 @@ const Footer = () => {
         <a href="https://www.ghibligen.org/" target="_blank" rel="noopener noreferrer">
         Ghibli Gen
         </a>
+        <span>·</span>
+        <a href="https://www.beautytestai.com/" target="_blank" rel="noopener noreferrer">
+        Beauty Test AI
+        </a>
+
 
 
     </footer>
